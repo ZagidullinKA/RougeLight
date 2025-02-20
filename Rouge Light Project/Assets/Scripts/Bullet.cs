a0c3a97f31fdaed8880285cd21815979ec9821af
ZagidullinKA <temnyi.oscuro2@gmail.com>
2025-02-21 01:46:58 +0300

    0.0.1

--- Rouge Light Project/Assets/Scripts/Bullet.cs
+++ Rouge Light Project/Assets/Scripts/Bullet.cs
@@ -55,6 +55,7 @@ public class Bullet : MonoBehaviour
     {
         damage = (int)Math.Round(baseDmg*CritChance(critDamageProbability));
         return damage;
+        // хуяк=хуяк и в коммит
     }
    
     public void DestroyBullet()
